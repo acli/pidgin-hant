3da38218a5dae9238dfe127d342ab1d515c9f5ca
Ambrose Li <ambrose.li@gmail.com>
2020-07-17 21:03:40 -0400

    Fixed class variable warnings, added header comments

--- Makefile
+++ Makefile
@@ -1,2 +1,17 @@
+all: reports zh_HK.po
+
+
 zh_HK.po: zh_TW.po tw2hk.pl
 	./tw2hk.pl < $< > zh_HK.tmp && mv zh_HK.tmp $@
+
+
+reports: fuzzies.out untranslated.out
+
+fuzzies.out: zh_TW.po distill.rb
+	./distill.rb --fuzzy < $< > $@
+
+untranslated.out: zh_TW.po distill.rb
+	./distill.rb --untranslated < $< > $@
+
+.PHONEY: all reports
+.DELETE_ON_ERRORS:
--- distill.rb
+++ distill.rb
@@ -1,4 +1,9 @@
 #!/usr/bin/ruby -K0
+#
+# Simple script to extract either untranslated strings or fuzzies from a PO file
+# so that we know what's still left to translate.
+#
+# Copyright (C) 2004, 2020 by Ambrose Li <ambrose.li@gmail.com>
 
 require 'getoptlong'
 
@@ -13,25 +18,25 @@ list_fuzzy_p = nil
 verbose_p = nil
 
 state = STATE_OTHER
-@@fuzzy_p = nil
-@@plural_p = nil
-@@plural_index = nil
-@@notes = nil
-@@directives = nil
-@@msgid = nil
-@@msgstr = nil
-@@strings = {}
-@@string_fuzzy_p = {}
-@@string_notes = {}
-@@string_directives = {}
+$fuzzy_p = nil
+$plural_p = nil
+$plural_index = nil
+$notes = nil
+$directives = nil
+$msgid = nil
+$msgstr = nil
+$strings = {}
+$string_fuzzy_p = {}
+$string_notes = {}
+$string_directives = {}
 
 def flush
-   if !@@msgid.nil? && !@@msgstr.nil?
-      @@strings[@@msgid] = @@msgstr
-      @@string_notes[@@msgid] = @@notes unless @@notes.nil?
-      @@string_directives[@@msgid] = @@directives unless @@directives.nil?
-      @@string_fuzzy_p[@@msgid] = true if @@fuzzy_p
-      @@notes = @@directives = @@fuzzy_p = @@plural_p = @@msgid = @@msgstr = nil
+   if !$msgid.nil? && !$msgstr.nil?
+      $strings[$msgid] = $msgstr
+      $string_notes[$msgid] = $notes unless $notes.nil?
+      $string_directives[$msgid] = $directives unless $directives.nil?
+      $string_fuzzy_p[$msgid] = true if $fuzzy_p
+      $notes = $directives = $fuzzy_p = $plural_p = $msgid = $msgstr = nil
    end
 end
 
@@ -73,37 +78,37 @@ loop {
    $stderr.puts "state=#{state.inspect}, s=#{s.inspect}" if verbose_p
 break if s.nil?
    s.chop!
-   if s !~ /^"(.*)\"$/ && state == STATE_MSGSTR_PLURAL && @@msgstr[@@plural_index].length == 0
-      $stderr.puts "Warning: blank plural string msgstr[#{@@plural_index}] for #{@@msgid.inspect}"
+   if s !~ /^"(.*)\"$/ && state == STATE_MSGSTR_PLURAL && $msgstr[$plural_index].length == 0
+      $stderr.puts "Warning: blank plural string msgstr[#{$plural_index}] for #{$msgid.inspect}"
    end
    if s =~ /^# (?:FIXME|NOTE|XXX)/
-      if @@notes.nil?
-	 @@notes = s
+      if $notes.nil?
+	 $notes = s
       else
-	 @@notes += "\n" + s
+	 $notes += "\n" + s
       end
    elsif s =~ /^#,/
       flush
-      @@directives = s
-      @@fuzzy_p = true if s =~ /fuzzy/
+      $directives = s
+      $fuzzy_p = true if s =~ /fuzzy/
    elsif s =~ /^msgid\s+\"(.*)\"\s*$/
       flush
-      @@msgid = $1
+      $msgid = $1
       state = STATE_MSGID
    elsif s =~ /^msgid_plural\s+\"(.*)\"\s*$/
       flush
-      @@msgid = $1
-      @@plural_p = true
-      @@plural_index = nil
+      $msgid = $1
+      $plural_p = true
+      $plural_index = nil
       state = STATE_MSGID_PLURAL
    elsif s =~ /^msgstr\s+\"(.*)\"\s*$/
-      @@msgstr = $1
+      $msgstr = $1
       state = STATE_MSGSTR
    elsif s =~ /^msgstr\[(\d+)\]\s+\"(.*)\"\s*$/
-      raise "msgstr[] found for non-plural msgid" unless @@plural_p
-      @@plural_index = $1.to_i
-      @@msgstr = [] if @@msgstr.nil? && @@plural_p
-      @@msgstr[@@plural_index] = $2
+      raise "msgstr[] found for non-plural msgid" unless $plural_p
+      $plural_index = $1.to_i
+      $msgstr = [] if $msgstr.nil? && $plural_p
+      $msgstr[$plural_index] = $2
       state = STATE_MSGSTR_PLURAL
    elsif s =~ /^msgid/
       $stderr.puts "Warning: unparsable msgid line (#{s})"
@@ -114,14 +119,14 @@ break if s.nil?
    elsif s =~ /^"(.*)\"$/
       case state
       when STATE_MSGID, STATE_MSGID_PLURAL
-	 @@msgid = "" if @@msgid.nil?
-	 @@msgid += $1
+	 $msgid = "" if $msgid.nil?
+	 $msgid += $1
       when STATE_MSGSTR
-	 @@msgstr = "" if @@msgstr.nil?
-	 @@msgstr += $1
+	 $msgstr = "" if $msgstr.nil?
+	 $msgstr += $1
       when STATE_MSGSTR_PLURAL
-	 @@msgstr[@@plural_index] = "" if @@msgstr[@@plural_index].nil?
-	 @@msgstr[@@plural_index] += $1
+	 $msgstr[$plural_index] = "" if $msgstr[$plural_index].nil?
+	 $msgstr[$plural_index] += $1
       else
 	 raise "Invalid continuation"
       end
@@ -138,16 +143,16 @@ break if s.nil?
 flush
 
 list_all_p = (list_fuzzy_p.nil? && list_untranslated_p.nil? )
-@@strings.sort.each { |a|
+$strings.sort.each { |a|
    msgid, msgstr = a
    if list_all_p
-      puts @@string_notes[msgid] unless @@string_notes[msgid].nil?
+      puts $string_notes[msgid] unless $string_notes[msgid].nil?
    end
    if msgstr.kind_of?( String )
-      if list_all_p || (list_fuzzy_p && @@string_fuzzy_p[msgid]) \
-		    || (list_untranslated_p && @@strings[msgid].length == 0)
+      if list_all_p || (list_fuzzy_p && $string_fuzzy_p[msgid]) \
+		    || (list_untranslated_p && $strings[msgid].length == 0)
 
-	 puts @@string_directives[msgid] unless @@string_directives[msgid].nil?
+	 puts $string_directives[msgid] unless $string_directives[msgid].nil?
 	 puts "msgid #{ present(msgid) }"
 	 puts "msgstr #{ present(msgstr) }"
       end
@@ -155,13 +160,13 @@ list_all_p = (list_fuzzy_p.nil? && list_untranslated_p.nil? )
       first_p = true
       i = 0
       msgstr.each {|s|
-	 if list_all_p || (list_fuzzy_p && @@string_fuzzy_p[msgid]) \
+	 if list_all_p || (list_fuzzy_p && $string_fuzzy_p[msgid]) \
 		       || (list_untranslated_p && s.length == 0)
 
 	    if first_p
 	       first_p = false
 	       puts "msgid_plural #{ present(msgid) }"
-	       puts @@string_directives[msgid] unless @@string_directives[msgid].nil?
+	       puts $string_directives[msgid] unless $string_directives[msgid].nil?
 	    end
 	    puts "msgstr[#{ i }] #{ present(s) }"
 	 end
